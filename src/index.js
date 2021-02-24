eb3af866071735a973d831747914976e8e0a51ea
Diana Holoborodko <diana.holoborodko@gmail.com>
2021-02-24 23:54:32 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array[0] === undefined){
+    return 0;
+  } else return Math.min(...array);
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array[0] === undefined){
+    return 0;
+  } else return Math.max(...array);
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array[0] === undefined || array.length === 0) {
+    return 0;
+  } else {
+    var avgValue = array([]);
+    return (array.reduce ((a, b) => (a + b))/array.length)
+  }
+  
 }
