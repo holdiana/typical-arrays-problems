c5b849aba9fcb2e0c4ff56101ed803c7e330b0bb
Diana Holoborodko <diana.holoborodko@gmail.com>
2021-03-15 14:10:22 +0200

    feat: add solution function

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 
 exports.min = function min (array) {
-  if (array[0] === undefined){
+  if (!array || array.length === 0){
     return 0;
   } else return Math.min(...array);
   
 }
 
 exports.max = function max (array) {
-  if (array[0] === undefined){
+  if ((!array || array.length === 0)){
     return 0;
   } else return Math.max(...array);
   
 }
 
 exports.avg = function avg (array) {
-  if (array[0] === undefined || array.length === 0) {
+  if ((!array || array.length === 0)) {
     return 0;
   } else {
-    var avgValue = array([]);
-    return (array.reduce ((a, b) => (a + b))/array.length)
+    let sumAvg = array.reduce (function(a, b) {
+      return (a + b);
+    });
+    return sumAvg / array.length;
   }
-  
 }
